14b8547b5e160ad91c4a49a13054982ef6aa2c2e
neha-13-7531 <neharikame13@gmail.com>
2024-12-25 16:15:24 +0530

    python .py

--- python .py
+++ python .py
@@ -98,3 +98,112 @@ for i in my_string:
 print("Reversed string:",str)
 output:
 Reversed string: gninrom dooG
+#Accept white space
+def process_words():
+    words=input("enter a sequence of whitespace separated word:") # accept input from user
+    unique_sorted_words=sorted(set(words.split()))  # split the word and removing duplicate and sorting them
+    print("         ".join(unique_sorted_words))
+process_words()
+output:enter a sequence of whitespace separated word: Neha Khushi Mohini
+Khushi         Mohini         Neha
+#find the 3 most common element in a list using a dictionary
+def find_top_3_element(x):
+    element_counts={}
+    for elem in x:
+        if elem in element_counts:
+            element_counts[elem] += 1
+        else:
+            element_counts[elem] = 1
+
+    sorted_elements=sorted(element_counts.items(),key=lambda y: y[1],reverse=True) # sort the element by count in descending order
+    top_3_elements=sorted_elements[:3]  #get top 3 element
+    return top_3_elements
+sample_list=[6,3,8,5,9,5,5,2,3,3,3]
+result=find_top_3_element(sample_list)
+print(result)
+output:
+[(3, 4), (5, 3), (6, 1)]
+#calculate the greatest common divisor(GCD) of two number using recursion
+def gcd(x,y):
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x%y)   #call the function
+num1=50
+num2=20
+result=gcd(num1,num2)
+print(f"The GCD of {num1} and {num2} is {result}.") 
+output:
+The GCD of 50 and 20 is 10.
+#write a function to count the frequency of each character in a given string
+def count_frequency(string):
+    frequency = {}   #create empty dictionary to store charecter frequency
+    for char in string:
+        if char in frequency:
+            frequency[char] += 1  # charecter is exits in the dictionary 
+        else:
+            frequency[char] = 1    # if not exits 
+    return frequency
+input_string="Good Morning"
+result = count_frequency(input_string)
+print(result)
+output:
+{'G': 1, 'o': 3, 'd': 1, ' ': 1, 'M': 1, 'r': 1, 'n': 2, 'i': 1, 'g': 1}
+#write a prgram to read a csv file and print contents line by line
+import csv
+def read_csv_file(file_path):
+    with open(file_path,mode='r') as file: # use open to open file and r mode for read file
+         csv_reader=csv.reader(file)   #csv_reader to read the file content
+         for line in csv_reader:
+            print(line)
+file_path='neha.csv'
+read_csv_file("C:\\Users\\hp\\Downloads\\neha.csv")
+output:
+['name', 'marks']
+['Anu', '90']
+['neha', '88']
+['moni', '75']
+['khushi', '80']
+#implement  a function to generate all permutation of a list
+def generate_permutation(x):
+    if len(x) <= 1:
+        return [x]
+    permutations = []
+    for i in range(len(x)):
+        current = x[i]
+        remaning=x[:i] + x[i+1:]  # remaning list without the current element
+        for permu in generate_permutation(remaning):# generate  permutation for reamning list
+            permutations.append([current] + permu)  # append for add all permutation
+    return permutations
+input_list=[10,11,12,13]
+result=generate_permutation(input_list)
+print(result)
+output:
+[[10, 11, 12, 13], [10, 11, 13, 12], [10, 12, 11, 13], [10, 12, 13, 11], [10, 13, 11, 12], [10, 13, 12, 11], [11, 10, 12, 13], [11, 10, 13, 12], [11, 12, 10, 13], [11, 12, 13, 10], [11, 13, 10, 12], [11, 13, 12, 10], [12, 10, 11, 13], [12, 10, 13, 11], [12, 11, 10, 13], [12, 11, 13, 10], [12, 13, 10, 11], [12, 13, 11, 10], [13, 10, 11, 12], [13, 10, 12, 11], [13, 11, 10, 12], [13, 11, 12, 10], [13, 12, 10, 11], [13, 12, 11, 10]]
+#generate a random dataset of integer and calculate the mean median and mode
+import random 
+from statistics import mean,median,mode  # import mean median and mode from statistics library
+def generate_dataset(size,min_val,max_val): # generate random dataset
+    return [random.randint(min_val,max_val) # use in a list comprehension to create a list of random integer
+for _ in range(size)]
+def calculate_statistics(data):
+    try:
+        mode_val=mode(data) # calculate mean,median,mode
+    except:
+        mode_value="No unique mode"
+    return {
+        'mean':mean(data),
+        'median':median(data),
+        'mode':mode_val
+    }
+dataset=generate_dataset(size=15,min_val=2,max_val=10)
+print(f"Dataset:{dataset}")
+stats=calculate_statistics(dataset)
+print(f"Mean:{stats['mean']}")
+print(f"Median:{stats['median']}")
+print(f"Mode:{stats['mode']}")   
+output:
+Dataset:[2, 9, 7, 10, 5, 9, 8, 4, 6, 7, 8, 6, 2, 5, 2]
+Mean:6
+Median:6
+Mode:2
